fce1c711f3a97e3a02e18ac762618ff5a22bc3b7
CaptainTyphoo <captaintyphoo@users.noreply.github.com>
2015-10-23 20:19:30 +0200

    Dateien von Homepage hinzugefügt

---
+++ matlab/Doppelpendel_LQR/Tuning_Parameter.m
@@ -0,0 +1,20 @@
+%% Gewichtung Regler
+parLQR.Ta = parDP.Ta;
+parKAL.Ta = parDP.Ta;
+if obere_untere_Ruhelage==1 %obere
+    
+    %LQR-Gewichtung disk
+    %parLQR.QEd=...
+    %parLQR.REd=...
+    %parLQR.NEd=...
+
+else %untere
+     
+    %LQR-Gewichtung disk
+    %parLQR.QEd=...
+    %parLQR.REd=...
+    %parLQR.NEd=...
+               
+end
+
+
