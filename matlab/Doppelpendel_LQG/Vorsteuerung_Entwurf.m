5777a274aa78272d495c6018704994740becc6a9
RobertCraven90 <danielhauer@e250-090.eduroam.tuwien.ac.at>
2015-10-27 11:20:14 +0100

    "Ausgabe unterdrückt von V und VVinf"

--- matlab/Doppelpendel_LQG/Vorsteuerung_Entwurf.m
+++ matlab/Doppelpendel_LQG/Vorsteuerung_Entwurf.m
@@ -4,8 +4,8 @@ function [parFF] = Vorsteuerung_Entwurf(sysk,parFF)
     en = [zeros(size(R,1)-1,1);1];
     v1 = (en'/R)';
     
-    V = [v1';v1'*sysk.a;v1'*sysk.a^2;v1'*sysk.a^3;v1'*sysk.a^4;v1'*sysk.a^5]
-    parFF.VVinv =  V^-1
+    V = [v1';v1'*sysk.a;v1'*sysk.a^2;v1'*sysk.a^3;v1'*sysk.a^4;v1'*sysk.a^5];
+    parFF.VVinv =  V^-1;
     Aw = V*sysk.a*parFF.VVinv;
     parFF.aa = [Aw(end,:),1];
 end
